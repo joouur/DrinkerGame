950e02faa886783397c6b98ebec98c91b72f4298
Jorge Moreno <nada.interesante@gmail.com>
2017-01-06 15:23:48 -0800

    Not really a change, just added some spaces

--- GiveOrTakeUW/Assets/Scripts/Managers/GDManager.cs
+++ GiveOrTakeUW/Assets/Scripts/Managers/GDManager.cs
@@ -26,10 +26,12 @@ namespace GameDrinker.Managers
         public GDDeck CurrentDeck;
 
         public GAMESTATUS Status;
+
         #region UnityAction Events
         public delegate void OnGDStart();
         public static event OnGDStart OnGameDrinkerStart;
         #endregion
+
         #endregion
 
         #region Class Methods
