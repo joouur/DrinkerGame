48a6e2e2e3e4e1d1e3bacfef3767bd84285b0bda
Jorge Moreno <nada.interesante@gmail.com>
2017-01-26 15:05:01 -0800

    UI Elements
    Added main Font
    Added Select Game Window
    Added Add Player Window
    Scripts need Modifications.

---
+++ GiveOrTakeUW/Assets/Scripts/GUI/Windows/SelectionWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDrinker.Tools;
+
+namespace GameDrinker.GentleUI
+{
+    public class SelectionWindow : GenericWindow
+    {
+        
+        public void SelectGiveOrTake()
+        {}
+        public void SelectKingsCup()
+        { }
+        public void SelectFTheDealer()
+        { }
+        public void SelectPiramid()
+        { }
+
+    }
+}
